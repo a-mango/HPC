54ec8ba9aa0336ebd15a1045b2679fb4c1858bdf
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-12 15:46:17 +0100

    feat: automate audios testing

---
+++ L1/audios/test_audios.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BIN_DIR=../code/bin/
+BIN_NAME=dtmf_encdec
+BIN_GOE=$BIN_NAME-goertzel
+BIN_FFT=$BIN_NAME-fft
+
+# Test all audio samples
+echo -e "\n===== TESTING AUDIO SAMPLES ====="
+for i in $(fd -e wav .); do
+  echo "FILE $i"
+
+  echo -en "\tGOE >>>> "
+  ./$BIN_DIR/$BIN_GOE decode "$i" | tail -1
+
+  echo -en "\tFFT >>>> "
+  ./$BIN_DIR/$BIN_FFT decode "$i" | tail -1
+done
