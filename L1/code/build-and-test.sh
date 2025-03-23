a2ac36499baafae95385505440a22399f7282271
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-23 07:00:24 +0100

    feat: add test suite script

---
+++ L1/code/build-and-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_DIR=build
+BIN_DIR=bin
+TEST_NAME=dtmf_test
+
+cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug
+cmake --build $BUILD_DIR -- -j8
+
+./$BIN_DIR/$TEST_NAME
