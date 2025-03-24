6f2adb2d6047a685f011974bfbba0cffa34e9cbd
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-24 01:43:47 +0100

    build: add desktop notification to test output

--- L1/code/build-and-test.sh
+++ L1/code/build-and-test.sh
@@ -5,10 +5,15 @@ set -e
 BUILD_DIR=build
 BIN_DIR=bin
 TEST_NAME=dtmf_test
+CMD="./$BIN_DIR/$TEST_NAME"
 
 export GTEST_COLOR=yes
 
 cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug
 cmake --build $BUILD_DIR -- -j8
 
-./$BIN_DIR/$TEST_NAME
+if $CMD; then
+  notify-send "Tests Passed" "All tests passed successfully"
+else
+  notify-send "Tests Failed" "Some tests failed" -u critical
+fi
