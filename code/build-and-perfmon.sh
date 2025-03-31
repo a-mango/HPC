b9315776cc2167f16dcb052e73d14a575b520e53
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-31 22:39:19 +0200

    feat: refactor likwid script

--- code/build-and-perfmon.sh
+++ code/build-and-perfmon.sh
@@ -6,8 +6,6 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 BUILD_DIR=$SCRIPT_DIR/build
 BIN_DIR=$SCRIPT_DIR/bin
 CMD_NAME=likwid-perfctr
-CMD_OPS="-a $BIN_DIR/dtmf_encdec_test encode input.txt output.wav"
-CMD="$CMD_NAME $CMD_OPS"
 
 BUILD_MODE=Release
 BUILD_OPTS="-DCMAKE_BUILD_TYPE=$BUILD_MODE -DENABLE_PERFMON=ON"
@@ -18,6 +16,12 @@ cmake --build $BUILD_DIR -- $MAKE_OPTS
 
 echo -n "1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ .!?,#" >input.txt
 
+CMD_OPS="-C 1 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec_test input.txt output.wav"
+CMD="$CMD_NAME $CMD_OPS"
+$CMD
+
+CMD_OPS="-C 1 -g MEM_DP -m $BIN_DIR/dtmf_encdec_test input.txt output.wav"
+CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
 rm -f input.txt output.wav
