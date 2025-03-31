603671f43af4effba1041d4fed323498054e97da
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-31 23:26:01 +0200

    feat: add titles to output

--- code/build-and-perfmon.sh
+++ code/build-and-perfmon.sh
@@ -19,26 +19,32 @@ cmake --build $BUILD_DIR -- $MAKE_OPTS
 echo -n "1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ .!?,#" >input.txt
 
 # Run encode and both versions of decode
+echo -e "\n===== ENCODE MAXPERF ====="
 CMD_OPS="-C 1 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec-fft encode input.txt output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxperf_encode.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
+echo -e "\n===== ENCODE MAXBAND ====="
 CMD_OPS="-C 1 -g MEM_DP -m $BIN_DIR/dtmf_encdec-fft encode input.txt output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxband_encode.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
+echo -e "\n===== ENCODE GOERTZEL ====="
 CMD_OPS="-C 1 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec-fft decode output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxperf_decode-fft.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
+echo -e "\n===== DECODE MAXBAND ====="
 CMD_OPS="-C 1 -g MEM_DP -m $BIN_DIR/dtmf_encdec-fft decode output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxband_decode-fft.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
+echo -e "\n===== DECODE GOERTZEL ====="
 CMD_OPS="-C 1 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec-goertzel decode output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxperf_decode-goertzel.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
 
+echo -e "\n===== DECODE MAXBAND ====="
 CMD_OPS="-C 1 -g MEM_DP -m $BIN_DIR/dtmf_encdec-goertzel decode output.wav" >"$(date +'%Y-%m-%dT%H:%M:%S%z')_maxband_decode-goertzel.log"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
