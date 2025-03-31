fd9926a55904bdaeff46ea6244649b7d059e1e8f
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-31 20:18:40 +0200

    fix: add missing input to dtmf binary

--- code/build-and-perfmon.sh
+++ code/build-and-perfmon.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 BIN_DIR=$SCRIPT_DIR/bin
 CMD_NAME=likwid-perfctr
-CMD_OPS="-C 0 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec_test"
+CMD_OPS="-C 0 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec_test input.txt output.wav"
 CMD="$CMD_NAME $CMD_OPS"
 
 BUILD_MODE=Release
@@ -15,4 +15,8 @@ MAKE_OPTS="-j8"
 cmake -S . -B $BUILD_DIR $BUILD_OPTS
 cmake --build $BUILD_DIR -- $MAKE_OPTS
 
+echo -n "1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ .!?,#" >input.txt
+
 $CMD
+
+rm -f input.txt output.wav
