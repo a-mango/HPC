04f4711499d12ba4608f88c980375d5ebf09bfe4
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-03-31 20:03:11 +0200

    feat: add perfmon test script

---
+++ code/build-and-perfmon.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "$(realpath $0)")
+BIN_DIR=$SCRIPT_DIR/bin
+CMD_NAME=likwid-perfctr
+CMD_OPS="-C 0 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec_test"
+CMD="$CMD_NAME $CMD_OPS"
+
+BUILD_MODE=Release
+BUILD_OPTS="-DCMAKE_BUILD_TYPE=$BUILD_MODE -DENABLE_PERFMON=ON"
+MAKE_OPTS="-j8"
+
+cmake -S . -B $BUILD_DIR $BUILD_OPTS
+cmake --build $BUILD_DIR -- $MAKE_OPTS
+
+$CMD
