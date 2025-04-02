e66b6526d112df7a9ec63cb2fc033b364dc1c5c5
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-04-02 20:01:51 +0200

    feat: refactor perfmon script

--- code/build-and-perfmon.sh
+++ code/build-and-perfmon.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+pretty_print() {
+  jq -r '"FLOPS/s: \(.MEM_DP.MEM_DP.Metric["DP [MFLOP/s]"].Values[0])\nMemory BW: \(.MEM_DP.MEM_DP.Metric["Memory bandwidth [MBytes/s]"].Values[0]) MB/s\nOI: \(.MEM_DP.MEM_DP.Metric["Operational intensity [FLOP/Byte]"].Values[0]) FLOP/Byte"' "$1"
+}
+
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 BUILD_DIR=$SCRIPT_DIR/build
 BIN_DIR=$SCRIPT_DIR/bin
@@ -11,60 +15,32 @@ BUILD_MODE=Release
 BUILD_OPTS="-DCMAKE_BUILD_TYPE=$BUILD_MODE -DENABLE_PERFMON=ON"
 MAKE_OPTS="-j8"
 
-cmake -S . -B $BUILD_DIR $BUILD_OPTS
-cmake --build $BUILD_DIR -- $MAKE_OPTS
+cmake -S . -B "$BUILD_DIR" "$BUILD_OPTS"
+cmake --build "$BUILD_DIR" -- "$MAKE_OPTS"
 
-echo -n "1234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ .!?,#" >input.txt
+python3 -c 'print("ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 *!?,." * 100, end="")' >input.txt
 
 NOW=$(date +'%Y-%m-%dT%H:%M:%S')
 mkdir -p "$SCRIPT_DIR/../log/perfmon/$NOW"
 pushd "$SCRIPT_DIR/../log/perfmon/$NOW"
 
-# Run encode and both versions of decode
-# echo -e "\n===== ENCODE MAXPERF ====="
-# CMD_OPS="-o maxperf_encode.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-fft encode $SCRIPT_DIR/input.txt $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-#
-# echo -e "\n===== ENCODE MAXBAND ====="
-# CMD_OPS="-o maxband_encode.json -C 2 -g MEM_DP -m $BIN_DIR/dtmf_encdec-fft encode $SCRIPT_DIR/input.txt $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-#
-# echo -e "\n===== DECODE FFT MAXPERF ====="
-# CMD_OPS="-o maxperf_decode-fft.json -C 2 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec-fft decode $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-#
-# echo -e "\n===== DECODE FFT MAXBAND ====="
-# CMD_OPS="-o maxband_decode-fft.json -C 2 -g MEM_DP -m $BIN_DIR/dtmf_encdec-fft decode $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-#
-# echo -e "\n===== DECODE GOERTZEL MAXPERF ====="
-# CMD_OPS="-o maxperf_decode-goertzel.json -C 2 -g FLOPS_DP -m $BIN_DIR/dtmf_encdec-goertzel decode $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-#
-# echo -e "\n===== DECODE GOERTZEL MAXBAND ====="
-# CMD_OPS="-o maxband_decode-goertzel.json -C 2 -g MEM_DP -m $BIN_DIR/dtmf_encdec-goertzel decode $SCRIPT_DIR/output.wav"
-# CMD="$CMD_NAME $CMD_OPS"
-# $CMD
-
 echo "Running encode benchmark..."
 CMD_OPS="-o encode.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-fft encode $SCRIPT_DIR/input.txt $SCRIPT_DIR/output.wav"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
+pretty_print encode.json
 
 echo "Running decode benchmark with FFT..."
-CMD_OPS="-o maxperf_decode-fft.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-fft decode $SCRIPT_DIR/output.wav"
+CMD_OPS="-o decode-fft.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-fft decode $SCRIPT_DIR/output.wav"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
+pretty_print decode-fft.json
 
 echo "Running decode benchmark with Goertzel..."
-CMD_OPS="-o maxperf_decode-goertzel.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-goertzel decode $SCRIPT_DIR/output.wav"
+CMD_OPS="-o decode-goe.json -C 2 -g HPC -m $BIN_DIR/dtmf_encdec-goertzel decode $SCRIPT_DIR/output.wav"
 CMD="$CMD_NAME $CMD_OPS"
 $CMD
+pretty_print decode-goe.json
 
 echo "Done! Reports saved in log/perfmon/$NOW"
 
