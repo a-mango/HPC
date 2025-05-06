7bb88a5d1617b0c451864965e1db9e5b41de839c
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2025-05-06 20:48:53 +0200

    feat(simd): cleanup header file

--- simd/segmented/include/k-means.h
+++ simd/segmented/include/k-means.h
@@ -9,7 +9,5 @@ typedef struct {
     int sum_b;
 } ClusterData;
 
-
-float distance(uint8_t *p1, uint8_t *p2);
-void  kmeans_pp(struct img_t *image, int num_clusters, uint8_t *centers);
-void  kmeans(struct img_t *image, int num_clusters);
+void kmeans_pp(struct img_t *image, int num_clusters, uint8_t *centers);
+void kmeans(struct img_t *image, int num_clusters);
